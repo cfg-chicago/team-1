35ec96fe425efe0871b6179001541097818358a7
Kevin Zheng <kevzheng@umich.edu>
2017-09-30 04:00:52 -0400

    Middle progress

--- app.py
+++ app.py
@@ -23,21 +23,70 @@ def index():
 def login_route():
     errors = []
     if request.method == 'POST':
-        user = request.form['username']
+        username = request.form['username']
         password = request.form['password']
 
-        cursor.execute('''SELECT password FROM User WHERE username = \"{}\"'''.format(user))
+        cursor.execute('''SELECT password FROM User WHERE username = \"{}\"'''.format(username))
         if cursor.rowcount != 0:
             data = cursor.fetchall()[0][0]
             if data == password:
-                return redirect(url_for('profile'))
+                cursor.execute('''SELECT bio FROM User WHERE username = \"{}\"'''.format(username))
+                bio = cursor.fetchall()[0][0]
+
+                cursor.execute('''SELECT journeyid FROM UserJourney WHERE username = \"{}\"'''.format(username))
+                userjournies = cursor.fetchall()
+
+                # Turn tuple of single-tuple items into list
+                userjournies = [x[0] for x in data] 
+                return redirect(url_for('profile_route', username=username,
+                                                   userjournies=userjournies,
+                                                   bio=bio))
             else:
                 errors.append("Wrong password")
         return render_template('login.html', errors= errors)
     return render_template('login.html', errors= errors)
 
+@app.route('/profile', methods=['GET', 'POST'])
+def profile_route():
+     username = request.args.get('username')
+     raise
+#    cursor.execute('''SELECT bio FROM User WHERE username = \"{}\"'''.format(username))
+#    cursor.execute('''SELECT bio FROM User WHERE username = \"{}\"'''.format(username))
+#    cursor.execute('''SELECT bio FROM User WHERE username = \"{}\"'''.format(username))
 
 
+@app.route('/journey', methods=['GET', 'POST'])
+def journey_route():
+    journey_selected = request.args.get('journey_selected')
+    
+    cursor.execute('''SELECT * FROM Journey''')
+    data = cursor.fetchall()
+    journies = {x[1]:x[0] for x in data}
+
+    stuff = []
+    for i in journies.values():
+        cursor.execute('''SELECT reactiondata FROM Reactions WHERE journeyid = {}'''.format(i))
+        data = cursor.fetchall()
+        stuff.append(data)
+        #raise
+
+    raise
+
+    if journey_selected == None:
+        return render_template('journey.html', journey_selected=None, journies=data)
+    else:
+        journies = request.args.get('journies')
+        reactions = request.args.get('reactions')
+
+    
+
+@app.route('/class')
+def class_route():
+    gradyear = request.args.get('gradyear')
+    if gradyear == None:
+        cursor.execute('''SELECT MAX(gradyear) FROM Class''')
+        #gradyear = 
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
